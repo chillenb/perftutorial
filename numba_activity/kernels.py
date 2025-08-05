13bdadcae8c456ae3a27b0d6c1bc37df28c430c3
chillenb <chillenbrand15@gmail.com>
2025-08-05 17:22:37 -0400

    answer key

--- numba_activity/kernels.py
+++ numba_activity/kernels.py
@@ -11,18 +11,42 @@ def ref_total_energy(coords):
     return U_sum / 2  # divide by 2 to account for double counting
 
 
-@njit(cache=True)
+@njit(cache=True, parallel=True, fastmath=True)
 def total_energy(coords):
     # sum_{i<j} U(r_i, r_j)
     tot_e = 0.0
     n = coords.shape[1]
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                dx = coords[0, i] - coords[0, j]
-                dy = coords[1, i] - coords[1, j]
-                dz = coords[2, i] - coords[2, j]
-                r2 = dx * dx + dy * dy + dz * dz
-                U = 1 / (1.0 + np.sqrt(r2))
-                tot_e += U
-    return tot_e / 2
+    TILESIZE = 16
+    ntiles = n // TILESIZE
+    # Tiles off the diagonal
+    for tile_i in prange(ntiles):
+        for tile_j in prange(tile_i):
+            for i in range(tile_i * TILESIZE, (tile_i + 1) * TILESIZE):
+                for j in range(tile_j * TILESIZE, (tile_j + 1) * TILESIZE):
+                    dx = coords[0, i] - coords[0, j]
+                    dy = coords[1, i] - coords[1, j]
+                    dz = coords[2, i] - coords[2, j]
+                    r2 = dx * dx + dy * dy + dz * dz
+                    U = 1 / (1.0 + np.sqrt(r2))
+                    tot_e += U
+    # Tiles along the diagonal
+    for tile_i in prange(ntiles):
+        for i in range(tile_i * TILESIZE, (tile_i + 1) * TILESIZE):
+            for j in range(tile_i * TILESIZE, (tile_i + 1) * TILESIZE):
+                if i < j:
+                    dx = coords[0, i] - coords[0, j]
+                    dy = coords[1, i] - coords[1, j]
+                    dz = coords[2, i] - coords[2, j]
+                    r2 = dx * dx + dy * dy + dz * dz
+                    U = 1 / (1.0 + np.sqrt(r2))
+                    tot_e += U
+    # Remainder
+    for i in range(ntiles * TILESIZE, n):
+        for j in range(i):
+            dx = coords[0, i] - coords[0, j]
+            dy = coords[1, i] - coords[1, j]
+            dz = coords[2, i] - coords[2, j]
+            r2 = dx * dx + dy * dy + dz * dz
+            U = 1 / (1.0 + np.sqrt(r2))
+            tot_e += U
+    return tot_e
--- profile_activity/run.py
+++ profile_activity/run.py
@@ -4,6 +4,8 @@ import numpy as np
 
 einsum = lib.einsum
 
+import pytblis
+
 from ref import get_sigma_diag_minimal_ref
 
 
@@ -33,21 +35,22 @@ def get_sigma_diag_minimal():
     omega[1:] = freqs[: (nw_sigma - 1)]
     emo = omega[None] + ef - mo_energy[:, None]
     sigma = np.zeros((norbs, nw_sigma))
+    Pi = None
     for w in range(nw):
         # Pi_inv = 1 - (1 - Pi)^{-1} - 1 = -[1 + (Pi - 1)^{-1}]
-        Pi = get_rho_response(freqs[w], mo_energy, Lpq[:, :nocc, nocc:])
+        Pi = get_rho_response(freqs[w], mo_energy, Lpq[:, :nocc, nocc:], out=Pi)
         Pi[range(naux), range(naux)] -= 1.0
         Pi_inv = np.linalg.inv(Pi)
         Pi_inv[range(naux), range(naux)] += 1.0
-        Qnm = einsum("Pnm, PQ -> Qnm", Lpq[:, orbs], Pi_inv)
-        Wmn = einsum("Qnm, Qmn -> mn", Qnm, Lpq[:, :, orbs])
+        Qnm = pytblis.contract("Pnm, PQ -> Qnm", Lpq[:, orbs], Pi_inv)
+        Wnm = einsum("Qnm, Qnm -> nm", Qnm, Lpq[:, orbs])
         g0 = wts[w] * emo / (emo**2 + freqs[w] ** 2)
-        sigma += einsum("mn, mw -> nw", Wmn, g0) / np.pi
+        pytblis.contract("nm, mw -> nw", Wnm, g0, alpha=1 / np.pi, beta=1.0, out=sigma)
     return sigma
 
 
 @line_profiler.profile
-def get_rho_response(omega, mo_energy, Lpq):
+def get_rho_response(omega, mo_energy, Lpq, out=None):
     """
     Compute density response function in auxiliary basis at freq iw.
     """
@@ -55,8 +58,8 @@ def get_rho_response(omega, mo_energy, Lpq):
     eia = mo_energy[:nocc, None] - mo_energy[None, nocc:]
     eia = eia / (omega**2 + eia * eia)
     # Response from both spin-up and spin-down density
-    Pia = Lpq * (eia * 4.0)
-    Pi = einsum("Pia, Qia -> PQ", Pia, Lpq)
+    Pia = Lpq * (4.0 * eia)
+    Pi = pytblis.contract("Pia, Qia -> PQ", Pia, Lpq, out=out)
     return Pi
 
 
